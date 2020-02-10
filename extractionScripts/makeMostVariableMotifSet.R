d7e9057c2dd87066eb7b1f672ba3f16892fe8c42
Eric Sanford <emsanford@gmail.com>
2020-02-10 15:19:37 -0500

    updated bug involving combining factors with strings

--- extractionScripts/makeMostVariableMotifSet.R
+++ extractionScripts/makeMostVariableMotifSet.R
@@ -10,11 +10,13 @@ library(SummarizedExperiment)
 library(BSgenome.Hsapiens.UCSC.hg38)
 register(MulticoreParam(4, progressbar = TRUE))
 
-manually_include_these_motifs <- c("ENSG00000126778_LINE2315_SIX1_I", "ENSG00000102974_LINE747_CTCF_D_N67")  # from homer de novo motif analysis on upregulated peaks
+# manually_include_these_motifs <- c("ENSG00000126778_LINE2315_SIX1_I", "ENSG00000102974_LINE747_CTCF_D_N67")  # from homer de novo motif analysis on upregulated peaks
+manually_include_these_motifs <- c("SIX1", "CTCF")  # from homer de novo motif analysis on upregulated peaks
+
 
 cmdargs = commandArgs(trailingOnly=TRUE)
 if (length(cmdargs) == 0) {
-  fragmentCounts <- read_rds(here('extractedData', 'final_diffPeaks_fragment_counts_mergedist250_peakwidth150_minNormFrags30_minFoldChange1.5.rds'))
+  fragmentCounts <- read_rds("/Users/emsanford/Dropbox (RajLab)/Shared_Eric/SIgnal_Integration/Analysis_SI2-SI4_github_testing/signal_integration_paper_scripts/extractedData/final_diffPeaks_fragment_counts_mergedist250_peakwidth150_minNormFrags30_minFoldChange1.5.rds")
   outputFile <- here("extractedData", "most_variable_motifs_Robject.rds")
 } else {
   fragmentCounts <- read_rds(cmdargs[1])
@@ -39,7 +41,9 @@ p <- p + geom_vline(xintercept = top_n_motifs_to_keep)
 tvar <- as.tibble(variability)
 
 TF.names.variabilitycutoff <- arrange(tvar, -variability)$name[1:top_n_motifs_to_keep]
-PWM.object.indices.bool <- sapply(strsplit(names(motifSet), "_"), function (x) x[3]) %in% c(TF.names.variabilitycutoff, manually_include_these_motifs)
+TF.names.manual.additions  <- tvar %>% filter(name %in% manually_include_these_motifs) %>% pull("name")
+TF.names.variabilitycutoff.plus.manual.additions <- union(TF.names.variabilitycutoff, TF.names.manual.additions)
+PWM.object.indices.bool <- sapply(strsplit(names(motifSet), "_"), function (x) x[3]) %in% TF.names.variabilitycutoff.plus.manual.additions
 PWM.object.indices.num  <- which(PWM.object.indices.bool)
 selected.PWM.objects <- motifSet[PWM.object.indices.num]
 saveRDS(selected.PWM.objects, file = outputFile)
