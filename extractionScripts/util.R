a1d3826d7f41ecac9f6358759f25ef606b671f23
Eric Sanford <emsanford@gmail.com>
2020-02-17 09:14:32 -0500

    removed automatic y axis scaling from util.R

--- extractionScripts/util.R
+++ extractionScripts/util.R
@@ -29,28 +29,27 @@ makeHistogramOfValues <- function(data.vector, categories.vector, xlim_lower, xl
   if (color.by.category) {
     if (y.axis.units == "density") {
       stackedBarHist <- ggplot(stackedBarHistTib, aes(x = intConstantHhistBin, y = ..prop.., group = 1, fill = intCategory)) +
-        geom_bar(stat="count", width = bin.radius * 2 * .90) +
-        ylim(0, (max(table(bin.values)[2:(length(table(bin.values)) - 2)]) * 1.05)/length(data.vector)) 
+        geom_bar(stat="count", width = bin.radius * 2 * .90)
+        # ylim(0, (max(table(bin.values)[2:(length(table(bin.values)) - 2)]) * 1.05)/length(data.vector)) 
     } else {
       stackedBarHist <- ggplot(stackedBarHistTib, aes(x = intConstantHhistBin, fill = intCategory)) +
-        geom_bar(stat="count", width = bin.radius * 2 * .90) +
-        ylim(0, max(table(bin.values)[2:(length(table(bin.values)) - 2)]) * 1.05)
+        geom_bar(stat="count", width = bin.radius * 2 * .90)
+        # ylim(0, max(table(bin.values)[2:(length(table(bin.values)) - 2)]) * 1.05)
     }
   } else {
     if (y.axis.units == "density") {
       stackedBarHist <- ggplot(stackedBarHistTib, aes(x = intConstantHhistBin, y = ..prop.., group = 1)) +
-        geom_bar(stat="count", width = bin.radius * 2 * .90) +
-        ylim(0, (max(table(bin.values)[2:(length(table(bin.values)) - 2)]) * 1.05)/length(data.vector)) 
+        geom_bar(stat="count", width = bin.radius * 2 * .90)
+        # ylim(0, (max(table(bin.values)[2:(length(table(bin.values)) - 2)]) * 1.05)/length(data.vector)) 
     } else {
       stackedBarHist <- ggplot(stackedBarHistTib, aes(x = intConstantHhistBin)) +
-        geom_bar(stat="count", width = bin.radius * 2 * .90) +
-        ylim(0, max(table(bin.values)[2:(length(table(bin.values)) - 2)]) * 1.05)
+        geom_bar(stat="count", width = bin.radius * 2 * .90)
+        # ylim(0, max(table(bin.values)[2:(length(table(bin.values)) - 2)]) * 1.05)
     }
   }
   
   stackedBarHist <- stackedBarHist +
     theme_classic(base_size = 12) + 
-    # ylim(0, 45) +
     xlab(xlabel) +
     ylab(ylabel) +
     ggtitle(paste0(plot.title, "\nleft-val ", table(bin.values)[1], " ", table(bin.values)[1]/length(data.vector),  
